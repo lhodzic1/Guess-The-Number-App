17e3f46f2a5f2a9b1b7b12a0e91ba527368ad55e
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-07-30 12:50:37 +0200

    game logic finished

---
+++ components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// this component can be wrapped around any content (passed as props.children)
+// additional style can be passed as props.style
+
+const Card = props => {
+    return (
+        <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+    )
+};
+
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 8,
+        padding: 20,
+        borderRadius: 8
+    }
+});
+
+export default Card;
---
+++ components/NumberContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import Colors from '../constants/colors';
+
+const NumberComponent = props => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.number}>{props.children}</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 2,
+        borderColor: Colors.accent,
+        padding: 10,
+        borderRadius: 8,
+        marginVertical: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    number: {
+        color: Colors.accent,
+        fontSize: 22
+    }
+
+});
+
+export default NumberComponent;
