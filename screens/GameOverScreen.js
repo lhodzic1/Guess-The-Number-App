bf8933d3b48b24b3bc8b2c4f83aee455f5dbe391
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-07-30 14:31:53 +0200

    game over image added

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,9 +8,17 @@ const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The Game is Over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    //za lokalne slike nije neophodno naglasiti width i height
+                    source={require('../assets/success.png')}
+                //    source={{uri: 'https://i.pinimg.com/564x/95/e1/1d/95e11d9286be186c867272aa5e1d1084.jpg'}}
+                    resizeMode='cover' />
+            </View>
             <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     )
 };
@@ -20,6 +28,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 300,
+        height: 300,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
