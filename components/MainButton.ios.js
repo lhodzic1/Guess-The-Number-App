ecde82ece3fe81891e6f694a568b55a66c46b545
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-08-04 18:10:51 +0200

    layout adjusted to different platforms

--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -4,9 +4,10 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../constants/colors';
 
 const MainButton = props => {
+
     return (
         <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-            <View style={{...styles.button, ...props.style}}>
+            <View style={{ ...styles.button, ...props.style }}>
                 <Text style={styles.buttonText}>{props.children}</Text>
             </View>
         </TouchableOpacity>
